4e0018a34af11a600480a31f86df1f8a311be515
thalisson-sousa <thalissonpop91@gmail.com>
2023-09-25 22:31:12 -0300

    feat: criando conexão com o banco e rotas para API

--- app.js
+++ app.js
@@ -7,9 +7,32 @@ const jet = require('jsonwebtoken');
 
 const app = express();
 
+// Config JSON Response
+app.use(express.json());
+
 // Open Route - Public Route
 app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo a nossa API!"})
 })
 
-app.listen(3000);
+// Registrar Usuario
+app.post('/auth/register', async(req, res) => {
+
+    const { name, email, password, confirmpassword } = req.body;
+
+    // validações
+    if(!name) {
+        res.status(202).json({msg: "O nome é Obrigatorio"})
+    }
+});
+
+// Credenciais
+const dbuser = process.env.DB_USER;
+const dbpassword = process.env.DB_PASS;
+
+// Conexão com o Banco
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpassword}@cluster0.qv0zx7r.mongodb.net/?retryWrites=true&w=majority`).then(() =>{
+    app.listen(3000);
+    console.log("Conectou ao Banco");
+}).catch((err) => console.log(err));
+
